a143be3a870e35abba085e7253953bb7049f6c66
paulobusch <paulo202015@outlook.com.br>
2020-05-06 22:53:05 -0300

    Entidade Brand

--- Vehicle.Domain/Mutations/Brands/Entities/Brand.cs
+++ Vehicle.Domain/Mutations/Brands/Entities/Brand.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Questor.Vehicle.Domain.Utils.Random;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Questor.Vehicle.Domain.Mutations.Brands.Entities
 {
+    [Table("brands")]
     public class Brand 
     {
+        [Required] [Key]
         public string Id { get; set; }
+
+        [Required] [MaxLength(200)] [Index("UQ_brands_name", IsUnique = true)]
         public string Name { get; set; }
     }
 }
