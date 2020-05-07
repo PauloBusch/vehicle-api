83475a7b2fc076a59ce3d530b1fc43880af49f58
paulobusch <paulo202015@outlook.com.br>
2020-05-06 23:30:07 -0300

    Implementado mutation de atualizar modelo

--- Vehicle.Domain/Mutations/Models/Mutations/UpdateModel.cs
+++ Vehicle.Domain/Mutations/Models/Mutations/UpdateModel.cs
@@ -1,4 +1,6 @@
-﻿using Questor.Vehicle.Domain.Utils.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Questor.Vehicle.Domain.Utils.Enums;
+using Questor.Vehicle.Domain.Utils.Interfaces;
 using Questor.Vehicle.Domain.Utils.Results;
 using System;
 using System.Collections.Generic;
@@ -12,14 +14,24 @@ namespace Questor.Vehicle.Domain.Mutations.Models.Mutations
         public string Id { get; set; }
         public string Name { get; set; }
 
-        public Task<MutationResult> ValidateAsync(VehicleMutationsHandler handler)
+        public async Task<MutationResult> ValidateAsync(VehicleMutationsHandler handler)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(Id)) return new MutationResult(EStatusCode.InvalidData, $"Parameter {nameof(Id)} is required");
+            if (string.IsNullOrWhiteSpace(Name) || Name.Length > 200) return new MutationResult(EStatusCode.InvalidData, $"Parameter {nameof(Name)} is invalid");
+            var exists = await handler.DbContext.Models.AnyAsync(m => m.Id == Id);
+            if (!exists) return new MutationResult(EStatusCode.NotFound, $"Model with {nameof(Id)} does not exists");
+            var existsName = await handler.DbContext.Models.AnyAsync(m => m.Id != Id && m.Name == Name);
+            if (existsName) return new MutationResult(EStatusCode.Conflict, $"Model with {nameof(Name)} already exists");
+            return null;
         }
 
-        public Task<MutationResult> ExecuteAsync(VehicleMutationsHandler handler)
+        public async Task<MutationResult> ExecuteAsync(VehicleMutationsHandler handler)
         {
-            throw new NotImplementedException();
+            var model = await handler.DbContext.Models.FindAsync(Id);
+            model.Name = Name;
+            handler.DbContext.Models.Update(model);
+            var rows = await handler.DbContext.SaveChangesAsync();
+            return new MutationResult(rows);
         }
     }
 }
