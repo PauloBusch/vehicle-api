9c38b0e51699a4b8d8eeed6e0dc752b2754b1b26
paulobusch <paulo202015@outlook.com.br>
2020-05-06 23:22:53 -0300

    Teste de uniade para atualizar modelo

---
+++ Vehicle.Domain/Mutations/Models/Mutations/UpdateModel.cs
@@ -0,0 +1,25 @@
+﻿using Questor.Vehicle.Domain.Utils.Interfaces;
+using Questor.Vehicle.Domain.Utils.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questor.Vehicle.Domain.Mutations.Models.Mutations
+{
+    public class UpdateModel : IMutation
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Task<MutationResult> ValidateAsync(VehicleMutationsHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MutationResult> ExecuteAsync(VehicleMutationsHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Vehicle.UnitTests/Tests/Models/UpdateModeltest.cs
+++ Vehicle.UnitTests/Tests/Models/UpdateModeltest.cs
@@ -5,47 +5,50 @@ using Questor.Vehicle.Domain.Utils.Random;
 using Questor.Vehicle.Domain.Utils.Results;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Vehicle.UnitTests.Tests.Models
 {
-    public class CreateModelTest : TestsBase
+    public class UpdateModeltest : TestsBase
     {
-        public CreateModelTest(VehicleFixture fixture) : base(fixture) { }
+        public UpdateModeltest(VehicleFixture fixture) : base(fixture) { }
 
-        public static IEnumerable<object[]> CreateModelData()
+        public static IEnumerable<object[]> UpdateModelData()
         {
             yield return new object[] { new MutationResult(EStatusCode.InvalidData), null, null };
             yield return new object[] { new MutationResult(EStatusCode.InvalidData), RandomId.NewId(), null };
             yield return new object[] { new MutationResult(EStatusCode.InvalidData), RandomId.NewId(), RandomId.NewId(250) };
+            yield return new object[] { new MutationResult(EStatusCode.NotFound), RandomId.NewId(), RandomId.NewId(200) };
             yield return new object[] { new MutationResult(EStatusCode.Conflict), RandomId.NewId(), RandomId.NewId(200) };
             yield return new object[] { new MutationResult(EStatusCode.Success), RandomId.NewId(), RandomId.NewId(200) };
         }
 
 
         [Theory]
-        [MemberData(nameof(CreateModelData))]
-        public async Task CreateModel(
+        [MemberData(nameof(UpdateModelData))]
+        public async Task UpdateModel(
             MutationResult expectedResult,
             string id,
             string name
-        ) { 
-            if (expectedResult.Status == EStatusCode.Conflict) { 
-                var model = new Model { Id = RandomId.NewId(), Name = name };
-                await MutationsDbContext.Models.AddAsync(model);
+        ) {
+            if (expectedResult.Status != EStatusCode.NotFound) { 
+                var model = new Model { Id = id ?? RandomId.NewId(), Name = name };
+                await MutationsDbContext.AddAsync(model);
                 await MutationsDbContext.SaveChangesAsync();
             }
-
-            var create = new CreateModel { Id = id, Name = name };
-            var result = await MutationsHandler.Handle(create);
+            if (expectedResult.Status == EStatusCode.Conflict) {
+                var model = new Model { Id = RandomId.NewId(), Name = name };
+                await MutationsDbContext.AddAsync(model);
+                await MutationsDbContext.SaveChangesAsync();
+            }
+            var update = new UpdateModel { Id = id, Name = name };
+            var result = await MutationsHandler.Handle(update);
             Assert.Equal(expectedResult.Status, result.Status);
             if (expectedResult.Status == EStatusCode.Success) { 
-                var modelDb = await MutationsDbContext.Models.FindAsync(create.Id);
-                Assert.Equal(create.Name, modelDb.Name);
+                var modelDb = await MutationsDbContext.Models.FindAsync(update.Id);
+                Assert.Equal(update.Name, modelDb.Name);
             }
         }
     }
