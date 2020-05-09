35babd0be9ab0803dc005877ebab4728cdee6137
paulobusch <paulo202015@outlook.com.br>
2020-05-09 13:51:06 -0300

    Teste de unidade para remover anúncio

---
+++ Vehicle.Domain/Mutations/Announcements/Mutations/DeleteAnnouncement.cs
@@ -0,0 +1,24 @@
+﻿using Questor.Vehicle.Domain.Utils.Interfaces;
+using Questor.Vehicle.Domain.Utils.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questor.Vehicle.Domain.Mutations.Announcements.Mutations
+{
+    public class DeleteAnnouncement : IMutation
+    {
+        public string Id { get; set; }
+
+        public Task<MutationResult> ValidateAsync(VehicleMutationsHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MutationResult> ExecuteAsync(VehicleMutationsHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vehicle.UnitTests/Tests/Announcements/DeleteAnnouncementTest.cs
@@ -0,0 +1,40 @@
+﻿using Questor.Vehicle.Domain.Mutations.Announcements.Mutations;
+using Questor.Vehicle.Domain.Utils.Enums;
+using Questor.Vehicle.Domain.Utils.Random;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using Xunit;
+
+namespace Vehicle.UnitTests.Tests.Announcements
+{
+    public class DeleteAnnouncementTest : TestsBase
+    {
+        public DeleteAnnouncementTest(VehicleFixture fixture) : base(fixture) { }
+    
+        public static IEnumerable<object[]> DeleteAnnouncementData()
+        {
+            yield return new object[] { EStatusCode.InvalidData, new DeleteAnnouncement { } };
+            yield return new object[] { EStatusCode.NotFound, new DeleteAnnouncement { Id = RandomId.NewId() } };
+            yield return new object[] { EStatusCode.Success, new DeleteAnnouncement { Id = RandomId.NewId() } };
+        }
+
+        [Theory]
+        [MemberData(nameof(DeleteAnnouncementData))]
+        public async void DeleteAnnouncement(
+            EStatusCode expectedStatus,
+            DeleteAnnouncement mutation
+        ) {
+            if (expectedStatus != EStatusCode.NotFound) 
+                EntitiesFactory.NewAnnouncement(id: mutation.Id).Save();
+        
+            var result = await MutationsHandler.Handle(mutation);
+            Assert.Equal(expectedStatus, result.Status);
+            if (expectedStatus == EStatusCode.Success) { 
+                var existsAnnouncement = await MutationsDbContext.Announcements.AnyAsync(a => a.Id == mutation.Id);
+                Assert.False(existsAnnouncement);
+            }
+        }
+    }
+}
