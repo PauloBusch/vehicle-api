4d96e7f140f69a4ca01853b12a749c782a7240ce
paulobusch <paulo202015@outlook.com.br>
2020-05-08 21:26:05 -0300

    Implementado mutation de atualizar veículo

--- Vehicle.Domain/Mutations/Vehicles/Mutations/UpdateVehicle.cs
+++ Vehicle.Domain/Mutations/Vehicles/Mutations/UpdateVehicle.cs
@@ -1,4 +1,6 @@
-﻿using Questor.Vehicle.Domain.Mutations.Vehicles.Entities.Enums;
+﻿using Microsoft.EntityFrameworkCore;
+using Questor.Vehicle.Domain.Mutations.Vehicles.Entities.Enums;
+using Questor.Vehicle.Domain.Utils.Enums;
 using Questor.Vehicle.Domain.Utils.Interfaces;
 using Questor.Vehicle.Domain.Utils.Results;
 using System;
@@ -17,14 +19,36 @@ namespace Questor.Vehicle.Domain.Mutations.Vehicles.Mutations
         public string ModelId { get; set; }
         public string BrandId { get; set; }
         
-        public Task<MutationResult> ValidateAsync(VehicleMutationsHandler handler)
+        public async Task<MutationResult> ValidateAsync(VehicleMutationsHandler handler)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(Id)) return new MutationResult(EStatusCode.InvalidData, $"Parameter {nameof(Id)} is required");
+            if (Year <= 1950) return new MutationResult(EStatusCode.InvalidData, $"Paramter {nameof(Year)} require upper 150");
+            if (!Enum.IsDefined(typeof(EFuel), FuelId)) return new MutationResult(EStatusCode.InvalidData, $"Parameter {nameof(FuelId)} is required");
+            if (!Enum.IsDefined(typeof(EColor), ColorId)) return new MutationResult(EStatusCode.InvalidData, $"Parameter {nameof(ColorId)} is required");
+            if (string.IsNullOrWhiteSpace(BrandId)) return new MutationResult(EStatusCode.InvalidData, $"Paramter {nameof(BrandId)} is required");
+            if (string.IsNullOrWhiteSpace(ModelId)) return new MutationResult(EStatusCode.InvalidData, $"Parameter {nameof(ModelId)} is required");
+            var exists = await handler.DbContext.Vehicles.AnyAsync(v => v.Id == Id);
+            if (!exists) return new MutationResult(EStatusCode.NotFound, $"Vehicle wtih {nameof(Id)} does not exists");
+            var existsModel = await handler.DbContext.Models.AnyAsync(m => m.Id == ModelId);
+            if (!existsModel) return new MutationResult(EStatusCode.NotFound, $"Model with id: {ModelId} does not exsits");
+            var existsBrand = await handler.DbContext.Brands.AnyAsync(b => b.Id == BrandId);
+            if (!existsBrand) return new MutationResult(EStatusCode.NotFound, $"Brand with id: {BrandId} does not exists");
+            return null;
         }
 
-        public Task<MutationResult> ExecuteAsync(VehicleMutationsHandler handler)
+        public async Task<MutationResult> ExecuteAsync(VehicleMutationsHandler handler)
         {
-            throw new NotImplementedException();
+            var vehicle = await handler.DbContext.Vehicles.FindAsync(Id);
+            vehicle.SetData(
+                year: Year,
+                color: ColorId,
+                fuel: FuelId,
+                modelId: ModelId,
+                brandId: BrandId
+            );
+            handler.DbContext.Update(vehicle);
+            var rows = await handler.DbContext.SaveChangesAsync();
+            return new MutationResult(rows);
         }
     }
 }
