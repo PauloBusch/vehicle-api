40aae2b0a5f1e3a78d3446f4103190e0dd2ed1d8
paulobusch <paulo202015@outlook.com.br>
2020-05-13 22:35:00 -0300

    Teste de unidade para atualizar reserva

---
+++ Vehicle.Domain/Mutations/Reservations/Mutations/UpdateReservation.cs
@@ -0,0 +1,27 @@
+﻿using Questor.Vehicle.Domain.Utils.Interfaces;
+using Questor.Vehicle.Domain.Utils.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questor.Vehicle.Domain.Mutations.Reservations.Mutations
+{
+    public class UpdateReservation : IMutation
+    {
+        public string Id { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public string AnnouncementId { get; set; }
+
+        public Task<MutationResult> ValidateAsync(VehicleMutationsHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MutationResult> ExecuteAsync(VehicleMutationsHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vehicle.UnitTests/Tests/Reservations/UpdateReservationTest.cs
@@ -0,0 +1,50 @@
+﻿using Questor.Vehicle.Domain.Mutations.Reservations.Mutations;
+using Questor.Vehicle.Domain.Utils.Enums;
+using Questor.Vehicle.Domain.Utils.Random;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Xunit;
+
+namespace Vehicle.UnitTests.Tests.Reservations
+{
+    public class UpdateReservationTest : TestsBase
+    {
+        public UpdateReservationTest(VehicleFixture fixture) : base(fixture) { }
+
+        public static IEnumerable<object[]> UpdateReservationData()
+        {
+            yield return new object[] { EStatusCode.InvalidData, new UpdateReservation { } };
+            yield return new object[] { EStatusCode.InvalidData, new UpdateReservation { Id = RandomId.NewId(), ContactName = RandomId.NewId(150) } };
+            yield return new object[] { EStatusCode.InvalidData, new UpdateReservation { Id = RandomId.NewId(), ContactName = RandomId.NewId(150) } };
+            yield return new object[] { EStatusCode.InvalidData, new UpdateReservation { Id = RandomId.NewId(), ContactName = RandomId.NewId(150), ContactPhone = RandomId.NewId(15) } };
+            yield return new object[] { EStatusCode.NotFound, new UpdateReservation { Id = RandomId.NewId(), ContactName = RandomId.NewId(150), ContactPhone = RandomId.NewId(15), AnnouncementId = RandomId.NewId() } };
+            yield return new object[] { EStatusCode.Success, new UpdateReservation { Id = RandomId.NewId(), ContactName = RandomId.NewId(150), ContactPhone = RandomId.NewId(15), AnnouncementId = RandomId.NewId() } };
+            yield return new object[] { EStatusCode.InvalidData, new UpdateReservation { Id = RandomId.NewId(), ContactName = RandomId.NewId(150), ContactPhone = RandomId.NewId(20), AnnouncementId = RandomId.NewId() } };
+            yield return new object[] { EStatusCode.InvalidData, new UpdateReservation { Id = RandomId.NewId(), ContactName = RandomId.NewId(160), ContactPhone = RandomId.NewId(15), AnnouncementId = RandomId.NewId() } };
+        }
+
+        [Theory]
+        [MemberData(nameof(UpdateReservationData))]
+        public async void UpdateReservation(
+            EStatusCode expectedStatus,
+            UpdateReservation mutation
+        ) {
+
+            EntitiesFactory.NewAnnouncement(id: mutation.AnnouncementId).Save();
+            if (expectedStatus != EStatusCode.NotFound)
+                EntitiesFactory.NewReservation(id: mutation.Id).Save();
+            var result = await MutationsHandler.Handle(mutation);
+            Assert.Equal(expectedStatus, result.Status);
+            if (expectedStatus == EStatusCode.Success) {
+                var reservationDb = await MutationsDbContext.Reservations
+                    .Where(r => r.Id == mutation.Id)
+                    .FirstOrDefaultAsync();
+                Assert.NotNull(reservationDb);
+                Assert.Equal(mutation.AnnouncementId, reservationDb.AnnouncementId);
+                Assert.Equal(mutation.ContactName, reservationDb.Contact.Name);
+                Assert.Equal(mutation.ContactPhone, reservationDb.Contact.Phone);
+            }
+        }
+    }
+}
